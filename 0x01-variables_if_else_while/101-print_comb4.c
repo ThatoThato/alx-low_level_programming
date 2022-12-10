674c8980bfcc00da5478bc498d72aad0231077a3
ThatoThato <thatomasike4@gmail.com>
2022-12-09 19:17:09 -0800

    task11

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int ones = '0';
+	int tens = '0';
+	int huns = '0';
+
+	for (huns = '0'; huns <= '9'; huns++)
+	{
+		for (tens = '0'; tens <= '9'; tens++)
+		{
+			for (ones = '0'; ones <= '9'; ones++)
+			{
+				if (!((ones == tens) || (tens == huns) || (tens > ones) || (huns > tens)))
+				{
+					putchar(huns);
+					putchar(tens);
+					putchar(ones);
+					if (!(ones == '9' && huns == '7' && tens == '8'))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
