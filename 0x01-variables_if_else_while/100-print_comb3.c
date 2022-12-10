d5992ca325a557f7ec74ae65781efdec1c1784bb
ThatoThato <thatomasike4@gmail.com>
2022-12-09 19:01:34 -0800

    task10

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,15 +11,15 @@ int main(void)
 	int n = '0';
 	int m = '0';
 
-	for (m = '0'; m <= '0'; m++)
+	for (m = '0'; m <= '9'; m++)
 	{
-		for (n = '0'; n <= '0'; n++)
+		for (n = '0'; n <= '9'; n++)
 		{
 			if (!((n == m) || (m > n)))
 			{
 				putchar(m);
 				putchar(n);
-				if (!(n == '0' && m == '0'))
+				if (!(n == '9' && m == '8'))
 				{
 					putchar(',');
 					putchar(' ');
