9fb2111d6aaf8ee1a2047cd6453aab1590571e6f
ThatoThato <thatomasike4@gmail.com>
2022-12-13 16:37:06 -0800

    task6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - compute value
+ * @t: the int to be computed
+ * Return: abs value
+ */
+
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
