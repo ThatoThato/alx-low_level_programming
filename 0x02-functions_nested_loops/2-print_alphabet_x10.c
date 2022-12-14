60a84966af848ce9857ef25e9488dfad34db28f4
ThatoThato <thatomasike4@gmail.com>
2022-12-13 16:07:15 -0800

    task2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - check the code
+ *
+ * Return - Always 0
+ */
+
+void print_alphabet_x10(void)
+{
+	int ten;
+	char c;
+
+	for (ten = 0; ten <= 9; ten++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+	}
+}
